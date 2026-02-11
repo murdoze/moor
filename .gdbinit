74170f077dba5f1aa143f28ca4575f0961332727
zoo6x <zoo6x@proton.me>
2026-02-11 07:55:25 +0100

    Follow forked children. Set pending breakpoints w/o asking

--- .gdbinit
+++ .gdbinit
@@ -2,8 +2,10 @@ set debuginfod enabled off
 set disable-randomization off
 set disassembly-flavor intel
 set disassemble-next-line on
+set follow-fork-mode child
+set breakpoint pending on
 
-handle SIGSEGV nostop noprint pass 
+#handle SIGSEGV nostop noprint pass 
 
 define tt
   target remote :1234
---
+++ moor.lua
@@ -0,0 +1,31 @@
+package.cpath = '/home/john/moor/?.so;' .. package.cpath
+
+local ffi = require("ffi")
+ffi.cdef[[
+  long long mur_add(long long a, long long b);
+
+
+  void vim_init();
+
+  typedef void (* emit_func_t)(int c); 
+  void vim_set_emit(emit_func_t ef);
+
+  void vim_run();
+
+]]
+local moor = ffi.load("moorst.so");
+
+-- print(tostring(tonumber(moor.mur_add(40, 2))))
+
+
+moor.vim_init()
+
+MOOR_OUT = "This is Forth output: "
+moor.vim_set_emit(function (c) MOOR_OUT = MOOR_OUT .. string.char(c) end)
+
+moor.vim_run()
+
+
+print("Moor!" .. MOOR_OUT)
+
+
